44bd7c0e5f7c3e8105dd0f6bb1a318deac480714
magava <magaava@gmail.com>
2016-03-07 18:54:44 +0400

    Messenger Async - not working version

---
+++ Tasks/Messenger/Magda Avagyan/Internship.Messenger.MyAsync_05032016/Intership.Messenger.Client/LoginForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intership.Messenger.Client
+{
+    public partial class LoginForm : Form
+    {
+        ServerConnection _serverConnection;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        public ServerConnection ServerConnection
+        {
+            get
+            {
+                return _serverConnection;
+            }
+
+           
+        }
+
+        async void ConnectAsync(object sender, EventArgs e)
+        {
+            await _serverConnection.Connect(txtServerIP.Text, Convert.ToInt32(txtServerPort.Text), txtNickname.Text);
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _serverConnection = new ServerConnection();
+            //_serverConnection.Connect(txtServerIP.Text, Convert.ToInt32(txtServerPort.Text), txtNickname.Text);
+            ConnectAsync(sender, e);
+            //TODO: Connect to server
+            this.Close();
+
+         
+        }
+
+
+    }
+}
---
+++ Tasks/Messenger/Magda Avagyan/Internship.Messenger.MyAsync_05032016/Intership.Messenger.Client/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intership.Messenger.Client
+{
+    public partial class MainForm : Form
+    {
+        ServerConnection _serverConnection;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            login.ShowDialog();
+            _serverConnection = login.ServerConnection;
+
+            _serverConnection.NewUserConnected += OnNewUserConnect;
+            _serverConnection.MessageReceived += OnNewMessage;
+        }
+
+
+
+        async private void OnNewMessage(Message message)
+        {
+            //this.BeginInvoke((Action)(() =>
+            //{
+            //    var clientNickName = _serverConnection.ConnectedUsers.First(x => x.UserId == message.ClientId).UserNickname;
+
+            //    lbMessages.Items.Add($"{clientNickName}: {message.MessageText}");
+            //}));
+
+            await Task.Run(() => {
+                var clientNickName = _serverConnection.ConnectedUsers.First(x => x.UserId == message.ClientId).UserNickname;
+
+                lbMessages.Items.Add($"{clientNickName}: {message.MessageText}");
+            });
+
+
+        }
+
+        async private Task OnNewMessageAsync(Message message)
+        {
+            await Task.Run(() => {
+                var clientNickName = _serverConnection.ConnectedUsers.First(x => x.UserId == message.ClientId).UserNickname;
+
+                 lbMessages.Items.Add($"{clientNickName}: {message.MessageText}");
+            });
+        }
+
+        async private void OnNewUserConnect(User user)
+        {
+            //this.BeginInvoke((Action)(() =>
+            //{
+            //    lbConnectedUsers.Items.Add(user.UserNickname);
+            //}));
+
+            await Task.Run( () => lbConnectedUsers.Items.Add(user.UserNickname)
+                );
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMessage.Text))
+                return;
+            _serverConnection.SendMessage(txtMessage.Text);
+
+            txtMessage.Clear();
+
+        }
+    }
+}
