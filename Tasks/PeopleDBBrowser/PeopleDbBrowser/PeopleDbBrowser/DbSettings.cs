28ccb87f1834838ffceea56653d32c291b4f4495
Gor90 <hovsepian@yandex.ru>
2016-03-10 01:53:15 -0800

    Project is Created

---
+++ Tasks/PeopleDBBrowser/PeopleDbBrowser/PeopleDbBrowser/DbSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternShip
+{
+    class DbSettings
+    {
+        public string Datasource { get; set; }
+        public bool IntegratedSecurity { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Initialcatalogue { get; set; }
+
+
+        public void Save()
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
