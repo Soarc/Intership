2863c8af4c16dfc2d634d5b55130b380bdb957f3
AngelaS95 <angelsalatyan@gmail.com>
2016-03-16 06:50:44 +0400

    Resigning save and load functions

--- Tasks/PeopleDBBrowser/PeopleDbBrowser/PeopleDbBrowser/Core/DbSettings.cs
+++ Tasks/PeopleDBBrowser/PeopleDbBrowser/PeopleDbBrowser/Core/DbSettings.cs
@@ -15,6 +15,8 @@ namespace Internship.PeopleDbBrowser.Core
         public string Password { get; set; }
         public string Initialcatalogue { get; set; }
 
+        public string ConnectionString { get; set; }
+
 
         public void Save()
         {
@@ -25,14 +27,23 @@ namespace Internship.PeopleDbBrowser.Core
             }
 
             //StreamReader strReader = new StreamReader(path);
-            string connsctionString = "Data Source =" + Datasource + "; Initial Catalog =" + Initialcatalogue + " ; Integrated Security =" + IntegratedSecurity +
-            ";User ID =" + Login + "; Password =" + Password + ";";
+            string connsctionString = "Data Source *** " + Datasource + "\n Initial Catalog *** " + Initialcatalogue + "\n Integrated Security *** " + IntegratedSecurity +
+            "\n User ID *** " + Login + "\n Password ***" + Password + "\n";
             File.AppendAllText(path, connsctionString+ "\n");
         }
 
         public void Load()
         {
             string[] lines = File.ReadAllLines(@"ConnectionString.txt");
+            string[] linesWithoutAsterics = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int astericsIndex = lines[i].IndexOf('*');
+                linesWithoutAsterics[i] = lines[i].Substring(lines[i][0], lines[i][astericsIndex]) + "=" +lines[i].Substring(lines[i][astericsIndex] + 4);
+
+                ConnectionString += linesWithoutAsterics[i]; 
+
+            }
         }
     }
 }
