180120221c2158a9ebef412933983898fd3d9b9e
Gor Rustamyan <gor.rustamyan@gmail.com>
2016-03-23 20:25:57 +0400

    Renamed RelayCommand Added Constructor

---
+++ Tasks/PeopleDBBrowser/PeopleDbBrowser/PeopleDbBrowser/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Internship.PeopleDbBrowser
+{
+    class RelayCommand : ICommand
+    {
+        Action _action;
+        public RelayCommand(Action action)
+        {
+            this._action = action;
+
+        }
+        public void Execute(object parameter)
+        {
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
